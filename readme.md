be3ee137c61e6dddc5785d271a234d507499a6a6
Casper <CasualX@users.noreply.github.com>
2019-09-17 11:53:34 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,135 @@
+# Projectile Solver
+
+## Hitting a moving target with a ballistic projectile
+
+The games I play come with simple ballistic physics for projectile-based weapons: the projectiles are fired with an initial speed and are only affected by gravity. The resulting trajectory is a ballistic trajectory.
+
+Because gravity is a harsh mistress literally every game 'cheats' this and fakes a much lower gravity constant for projectiles than is used for players. Without this hack projectiles are either pathethic or need a very high velocity to compensate.
+
+In this article I present a method of calculating where to aim your weapon which fires a projectile under simple gravity to hit an arbitrary moving target in 3D space. For a video of the result, scroll down to the bottom.
+
+Before we tackle this problem let's consider a more simple example: hitting a stationary target. Let's simplify even further to only consider the 2D case:
+
+Consider the location from which the projectile is fired at `[0, 0]` and we're trying to hit a target at `[x, y]`. As luck would have it [wikipedia's Projectile motion article](https://en.wikipedia.org/wiki/Projectile_motion#Angle_%7F'%22%60UNIQ--postMath-0000003A-QINU%60%22'%7F_required_to_hit_coordinate_\(x,y\)) has the formula for calculating the angle to shoot at:
+
+![](https://wikimedia.org/api/rest_v1/media/math/render/svg/2c5c375af9d89e403690dceeb6d074eab6ed27fe)
+
+An interesting feature is that this equation has either zero, one or two solutions. Intuitively these correspond with whether the projectile has enough speed to even reach the target and if it's too fast we can either shoot directly at the target or lob it up high and hit it on the way back down.
+
+How do we know which of the two solutions to pick? First if the value under the square root is negative then there are no solutions, the target is too far away. Then the final value is put under `atan` to get the angle. The larger the slope value given to `atan` is, the closer the angle is to 90°. A lower slope results in a lower angle (assuming all positive values). Thus the lower slope is the optimal angle and the higher slope lobs the projectile up high.
+
+Finally we can calculate the time the projectile takes to reach the target. This is independent of gravity and is simply derived from the horizontal distance.
+
+```rust
+fn optimal_angle(x: f32, y: f32, v0: f32, g: f32) -> Option<f32> {
+	let root = v0 * v0 * v0 * v0 - g * (g * x * x + 2.0 * y * v0 * v0);
+	if root < 0.0 {
+		return None;
+	}
+	let root = f32::sqrt(root);
+	let angle = f32::atan((v0 * v0 - root) / (g * x));
+	Some(angle)
+}
+
+fn lob_angle(x: f32, y: f32, v0: f32, g: f32) -> Option<f32> {
+	let root = v0 * v0 * v0 * v0 - g * (g * x * x + 2.0 * y * v0 * v0);
+	if root < 0.0 {
+		return None;
+	}
+	let root = f32::sqrt(root);
+	let angle = f32::atan((v0 * v0 + root) / (g * x));
+	Some(angle)
+}
+
+fn travel_time(x: f32, angle: f32, v0: f32) -> f32 {
+	x / (f32::cos(angle) * v0)
+}
+```
+
+Everything is better with visualizations! Here's the result of the formulas:
+
+![](img/fig1.svg)
+
+Legend:
+* The projectile is fired with `v0 = 650 u/s`, `g = 400 u/s²`.
+* Green is the target at position `[650, 150]`.
+* Blue is the optimal path aimed at `34.27°` and reaches the target in `1.21 s`.
+* Red lobs the projectile at `68.73°` and reaches the target in `2.76 s`.
+
+So far so good, we can hit stationary target in 2D. In fact, this also solves the problem of hitting a stationary target in 3D with a clever framing of the problem:
+
+In this simplified world where projectiles are only affected by gravity, consider the vertical plane passing through the player and the target. The projectile fired by the player travels in this plane! Thus we can construct a 2D setup which when solved results in an angle aimed at the target and pitched with the resulting angle from the 2D solution.
+
+(still working on 3D animations!)
+
+So how do we hit a moving target? If we aimed at the target as if he stood still, when the projectile reaches the target at t<sub>hit</sub>. However at this point the target would have moved some distance away. Now let t<sub>guess</sub> start at `0 s` and increment in steps of, say, `0.01 s`. Predict the target's position at t<sub>guess</sub> and fire a projectile at this position. Compare the projectile's travel time t<sub>hit</sub> with our chosen t<sub>guess</sub>. Initially t<sub>guess</sub> is less than t<sub>hit</sub> but as t<sub>guess</sub> increases these values converge (or not in case the target is not reachable). When t<sub>guess</sub> first gets larger (but still close to) t<sub>hit</sub> we have a fairly accurate estimation of when and where a projectile, when fired at this position, will hit the target.
+
+All of this hinges on a good prediction of the target. In the following demo I will use simple linear extrapolation, but quadratic extrapolation to simulate gravity works just as well. This is a very crude approximation of the future and gets less reliable the further in the future is predicted. We can put an arbitrary cap where the accuracy of this extrapolation becomes too inaccurate. For the purpose of this demo it doesn't matter as we'll make the target move predictably.
+
+The algorithm in code:
+
+```rust
+struct Target {
+	position: [f32; 2],
+	velocity: [f32; 2],
+	gravity: f32,
+}
+impl Target {
+	fn predict(&self, time: f32) -> [f32; 2] {
+		let x = self.position[0] + self.velocity[0] * time;
+		let y = self.position[1] + self.velocity[1] * time - self.gravity * time * time * 0.5;
+		[x, y]
+	}
+}
+
+struct Weapon {
+	speed: f32,
+	gravity: f32,
+}
+
+const MAX_TIME: f32 = 5.5;
+const TIME_STEP: f32 = 0.01;
+
+struct Solver {
+	target: Target,
+	weapon: Weapon,
+}
+impl Solver {
+	fn solve(&self) -> Option<f32> {
+		let mut target_time = 0.0;
+		while target_time < MAX_TIME {
+			let target_pos = self.target.predict(target_time);
+			let sol_angle = self.angle(target_pos)?;
+			let sol_time = travel_time(target_pos[0], sol_angle, self.weapon.speed);
+			if sol_time < target_time {
+				return Some(sol_angle);
+			}
+			target_time += TIME_STEP;
+		}
+		None
+	}
+	fn angle(&self, pos: [f32; 2]) -> Option<f32> {
+		optimal_angle(pos[0], pos[1], self.weapon.speed, self.weapon.gravity)
+	}
+}
+```
+
+And visualized:
+
+![](img/fig2.svg)
+
+Legend:
+* The projectile is fired with `v0 = 600 u/s`, `g = 400 u/s²`.
+* Green is the target at position `[450, 0]` and is moving with velocity `[100, 50]` in a linear path.
+* Blue is the optimal path aimed at `24.96°` and reaches the target in `1.01 s`.
+* Red lobs the projectile at `61.17°` and reaches the target in `2.38 s`.
+
+The above animation shows the solver in 2D action but is trivially extended to 3D. Plug in an appropriate target position predictor and construct the 2D setup before solving.
+
+The animations were made with custom code and rendering to svg with [format_xml](https://github.com/CasualX/format_xml). My personal goal with this article is to experiment with quickly and simply visualizing algorithms and using Rust + SVG as my canvas.
+
+This isn't something new, but I wanted to write this down for future reference. Years ago I made a video demonstrating the results of my efforts in [Team Fortress 2](https://en.wikipedia.org/wiki/Team_Fortress_2):
+
+[![](https://img.youtube.com/vi/MHZ35b_q_Gc/0.jpg)](https://www.youtube.com/watch?v=MHZ35b_q_Gc)
+
+Thanks for reading!
---
+++ src/main.rs
@@ -0,0 +1,228 @@
+#![recursion_limit="512"]
+
+use std::fmt;
+
+macro_rules! xprintln {
+	($($tt:tt)*) => {
+		println!("{}", format_xml::format_xml!($($tt)*));
+	};
+}
+macro_rules! xwrite {
+	($w:expr, $($tt:tt)*) => {
+		write!($w, "{}", format_xml::format_xml!{$($tt)*})
+	};
+}
+
+fn main() {
+	xprintln! {
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>"Projectile Solver"</title>
+		</head>
+			<body>
+				|f| {stationary(f)}
+				|f| {moving(f)}
+			</body>
+		</html>
+	}
+}
+
+/// Generate a sequence of timestamps from `0` to `until` with increments of `0.05` ending with the value of `until`.
+fn seq(until: f32) -> impl Clone + Iterator<Item = f32> {
+	let step = 0.05;
+	(0..)
+		.map(move |i| i as f32 * step)
+		.take_while(move |&time| time < until + step)
+		.map(move |time| f32::min(time, until))
+}
+/// Render an iterator of 2D points to SVG path syntax.
+fn path(mut input: impl Iterator<Item = [f32; 2]>) -> String {
+	let start = input.next().unwrap();
+	let mut s = format!("M{:.2} {:.2}", start[0], start[1]);
+	use std::fmt::Write;
+	for p in input {
+		let _ = s.write_fmt(format_args!("L{:.2} {:.2}", p[0], p[1]));
+	}
+	return s;
+}
+
+fn moving(f: &mut dyn fmt::Write) -> fmt::Result {
+	let weapon = Weapon {
+		speed: 600.0,
+		gravity: 400.0,
+	};
+	let target = Target {
+		position: [450.0, 0.0],
+		velocity: [100.0, 50.0],
+		gravity: 0.0,
+	};
+	let s1 = Solver::lob(weapon, target).solve().unwrap();
+	let s2 = Solver::optimal(weapon, target).solve().unwrap();
+	xwrite! { f,
+		<svg width="800" height="450" viewBox="-50 -600 800 450" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+			<g transform="translate(0, -200) scale(1,-1)">
+				<line x0="-1000" x1="2000" y0="0" y1="0" stroke="black" vector-effect="non-scaling-stroke" shape-rendering="crispEdges" />
+				<line y0="-1000" y1="1000" x0="0" x1="0" stroke="black" vector-effect="non-scaling-stroke" shape-rendering="crispEdges" />
+				<path d={path(seq(s1.time + 0.3).map(|time| target.predict(time)))} fill="none" stroke="green" style="opacity: 0.5;" id="A_s0" />
+				<path d={path(seq(s1.time).map(|time| weapon.fire(s1.angle, time)))} fill="none" stroke="red" style="opacity: 0.5;" id="A_s1" />
+				<path d={path(seq(s2.time).map(|time| weapon.fire(s2.angle, time)))} fill="none" stroke="blue" style="opacity: 0.5;" id="A_s2" />
+				<circle r="5" fill="green">
+					<animateMotion dur={s1.time + 0.3} fill="freeze" begin="0s;A_m.end+1s">
+					<mpath xlink:href="#A_s0"></mpath>
+					</animateMotion>
+				</circle>
+				<circle r="5" fill="red">
+					<animateMotion id="A_m" dur={s1.time} fill="freeze" begin="0s;A_m.end+1s">
+					<mpath xlink:href="#A_s1"></mpath>
+					</animateMotion>
+				</circle>
+				<circle r="5" fill="blue">
+					<animateMotion dur={s2.time} fill="freeze" begin="0s;A_m.end+1s">
+					<mpath xlink:href="#A_s2"></mpath>
+					</animateMotion>
+				</circle>
+			</g>
+		</svg>
+	}
+}
+
+fn stationary(f: &mut dyn fmt::Write) -> fmt::Result {
+	let weapon = Weapon {
+		speed: 650.0,
+		gravity: 400.0,
+	};
+	let target = Target {
+		position: [650.0, 150.0],
+		velocity: [0.0, 0.0],
+		gravity: 0.0,
+	};
+	let s1 = Solver::lob(weapon, target).solve().unwrap();
+	let s2 = Solver::optimal(weapon, target).solve().unwrap();
+	xwrite! { f,
+		<svg width="800" height="450" viewBox="-50 -600 800 450" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+			<g transform="translate(0, -200) scale(1,-1)">
+				<line x0="-1000" x1="2000" y0="0" y1="0" stroke="black" vector-effect="non-scaling-stroke" shape-rendering="crispEdges" />
+				<line y0="-1000" y1="1000" x0="0" x1="0" stroke="black" vector-effect="non-scaling-stroke" shape-rendering="crispEdges" />
+				<circle cx={target.position[0]} cy={target.position[1]} r="5" fill="green" />
+				<path d={path(seq(s1.time).map(|time| weapon.fire(s1.angle, time)))} fill="none" stroke="red" style="opacity: 0.5;" id="B_s1" />
+				<path d={path(seq(s2.time).map(|time| weapon.fire(s2.angle, time)))} fill="none" stroke="blue" style="opacity: 0.5;" id="B_s2" />
+				<circle r="5" fill="red">
+					<animateMotion id="B_m" dur={s1.time} fill="freeze" begin="0s;B_m.end+1s">
+					<mpath xlink:href="#B_s1"></mpath>
+					</animateMotion>
+				</circle>
+				<circle r="5" fill="blue">
+					<animateMotion dur={s2.time} fill="freeze" begin="0s;B_m.end+1s">
+					<mpath xlink:href="#B_s2"></mpath>
+					</animateMotion>
+				</circle>
+			</g>
+		</svg>
+	}
+}
+
+//----------------------------------------------------------------
+
+#[derive(Copy, Clone, Debug, Default)]
+pub struct Target {
+	pub position: [f32; 2],
+	pub velocity: [f32; 2],
+	pub gravity: f32,
+}
+impl Target {
+	/// Extrapolates the target under freefall `time` seconds into the future.
+	pub fn predict(&self, time: f32) -> [f32; 2] {
+		let x = self.position[0] + self.velocity[0] * time;
+		let y = self.position[1] + self.velocity[1] * time - self.gravity * time * time * 0.5;
+		[x, y]
+	}
+}
+
+/// Projectile Weapon stats
+#[derive(Copy, Clone, Debug, Default)]
+pub struct Weapon {
+	/// Initial speed of the projectile when fired
+	pub speed: f32,
+	/// Initial gravity of the projectile when fired
+	pub gravity: f32,
+}
+impl Weapon {
+	/// Simulates firing at given angle and returns the projectile's position at given time.
+	pub fn fire(&self, angle: f32, time: f32) -> [f32; 2] {
+		let v0 = self.speed;
+		let g = self.gravity;
+		let vel_x = angle.cos() * v0;
+		let vel_y = angle.sin() * v0;
+		[vel_x * time, vel_y * time - 0.5 * g * time * time]
+	}
+}
+
+/// Projectile aiming solution.
+#[derive(Copy, Clone, Debug, Default)]
+pub struct Solution {
+	/// Fire at this angle.
+	pub angle: f32,
+	/// Projectile will hit the target at this time in the future.
+	pub time: f32,
+}
+
+/// Give up when the projectile fails to connect with the target this time in the future.
+pub const MAX_TIME: f32 = 5.5;
+/// Time in seconds between each step of the algorithm.
+pub const TIME_STEP: f32 = 0.01;
+
+/// Projectile aim solver.
+#[derive(Copy, Clone, Debug)]
+pub struct Solver {
+	weapon: Weapon,
+	target: Target,
+	solve2d: fn(f32, f32, f32, f32) -> Option<Solution>,
+}
+impl Solver {
+	pub fn optimal(weapon: Weapon, target: Target) -> Solver {
+		Solver { weapon, target, solve2d: Self::solve2d_optimal }
+	}
+	pub fn lob(weapon: Weapon, target: Target) -> Solver {
+		Solver { weapon, target, solve2d: Self::solve2d_lob }
+	}
+	pub fn solve(&self) -> Option<Solution> {
+		let mut target_time = 0.0;
+		while target_time < MAX_TIME {
+			let target_pos = self.target.predict(target_time);
+			let sol = self.solve2d(target_pos)?;
+			if sol.time < target_time {
+				return Some(sol);
+			}
+			target_time += TIME_STEP;
+		}
+		None
+	}
+	fn solve2d(&self, pos: [f32; 2]) -> Option<Solution> {
+		(self.solve2d)(pos[0], pos[1], self.weapon.speed, self.weapon.gravity)
+	}
+	// Optimal solution with the lowest projectile travel time.
+	// Returns None if the projectile cannot reach the target.
+	fn solve2d_optimal(x: f32, y: f32, v0: f32, g: f32) -> Option<Solution> {
+		let root = v0 * v0 * v0 * v0 - g * (g * x * x + 2.0 * y * v0 * v0);
+		if root < 0.0 {
+			return None;
+		}
+		let root = f32::sqrt(root);
+		let angle = f32::atan((v0 * v0 - root) / (g * x));
+		let time = x / (f32::cos(angle) * v0);
+		Some(Solution { angle, time })
+	}
+	// Lobbing solution with the highest projectile travel time.
+	// Returns None if the projectile cannot reach the target.
+	fn solve2d_lob(x: f32, y: f32, v0: f32, g: f32) -> Option<Solution> {
+		let root = v0 * v0 * v0 * v0 - g * (g * x * x + 2.0 * y * v0 * v0);
+		if root < 0.0 {
+			return None;
+		}
+		let root = f32::sqrt(root);
+		let angle = f32::atan((v0 * v0 + root) / (g * x));
+		let time = x / (f32::cos(angle) * v0);
+		Some(Solution { angle, time })
+	}
+}
